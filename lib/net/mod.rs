cafc800dff5c480fb707160d6deb3e3a48c4161d
octobocto <bo@jalborg.com>
2026-09-09 10:48:13 -0700

    net: register peers before close events

--- lib/net/mod.rs
+++ lib/net/mod.rs
@@ -314,19 +314,31 @@ impl Net {
         &self,
         addr: SocketAddr,
         peer_connection_handle: PeerConnectionHandle,
+        info_rx: mpsc::UnboundedReceiver<PeerConnectionInfo>,
     ) -> Result<(), error::AlreadyConnected> {
         tracing::trace!(%addr, "add active peer: starting");
         let mut active_peers_write = self.active_peers.write();
         match active_peers_write.entry(addr) {
             hash_map::Entry::Occupied(_) => {
                 tracing::error!(%addr, "add active peer: already connected");
-                Err(error::AlreadyConnected(addr))
+                return Err(error::AlreadyConnected(addr));
             }
             hash_map::Entry::Vacant(active_peer_entry) => {
                 active_peer_entry.insert(peer_connection_handle);
-                Ok(())
             }
         }
+        drop(active_peers_write);
+        tokio::spawn({
+            let info_rx = StreamNotifyClose::new(info_rx)
+                .map(move |info| Ok((addr, info)));
+            let peer_info_tx = self.peer_info_tx.clone();
+            async move {
+                if let Err(_send_err) = info_rx.forward(peer_info_tx).await {
+                    tracing::error!(%addr, "Failed to send peer connection info");
+                }
+            }
+        });
+        Ok(())
     }
 
     pub fn remove_active_peer(&self, addr: SocketAddr) {
@@ -390,7 +402,6 @@ impl Net {
                 }
             }
         }
-        let peer_addr = resolved_addr.as_peer_address().to_owned();
         let (addr, connecting) = loop {
             let addr = SocketAddr::new(
                 resolved_addr.first_ip_addr(),
@@ -422,23 +433,7 @@ impl Net {
 
         let (connection_handle, info_rx) =
             peer::connect(connecting, connection_ctxt);
-        tracing::trace!("connect peer: spawning info rx");
-        tokio::spawn({
-            let info_rx = StreamNotifyClose::new(info_rx)
-                .map(move |info| Ok((addr, info)));
-            let peer_info_tx = self.peer_info_tx.clone();
-            async move {
-                if let Err(_send_err) = info_rx.forward(peer_info_tx).await {
-                    tracing::error!(
-                        addr = %peer_addr,
-                        "Failed to send peer connection info",
-                    );
-                }
-            }
-        });
-
-        tracing::trace!("connect peer: adding to active peers");
-        self.add_active_peer(addr, connection_handle)?;
+        self.add_active_peer(addr, connection_handle, info_rx)?;
         Ok(())
     }
 
@@ -606,18 +601,7 @@ impl Net {
         };
         let (connection_handle, info_rx) =
             peer::handle(connection_ctxt, connection);
-        tokio::spawn({
-            let info_rx = StreamNotifyClose::new(info_rx)
-                .map(move |info| Ok((addr, info)));
-            let peer_info_tx = self.peer_info_tx.clone();
-            async move {
-                if let Err(_send_err) = info_rx.forward(peer_info_tx).await {
-                    tracing::error!(%addr, "Failed to send peer connection info");
-                }
-            }
-        });
-        // TODO: is this the right state?
-        self.add_active_peer(addr, connection_handle)?;
+        self.add_active_peer(addr, connection_handle, info_rx)?;
         Ok(Some(addr))
     }
 
@@ -744,6 +728,42 @@ mod test {
             HashSet::new(),
         )?;
         Ok((temp_dir, env, net, info_rx))
+    }
+
+    #[tokio::test]
+    async fn rejected_duplicate_has_no_peer_close_event() -> anyhow::Result<()>
+    {
+        let (_temp_dir, env, net, info_rx) = temp_net("peer-duplicate")?;
+        let (remote, _) =
+            make_server_endpoint((Ipv4Addr::LOCALHOST, 0).into())?;
+        let addr = remote.local_addr()?;
+        net.connect_peer(env.clone(), addr.into())?;
+        let connection_ctxt = super::PeerConnectionCtxt {
+            env,
+            archive: net.archive.clone(),
+            magic_bytes: net.magic_bytes,
+            resolved_address: addr.into(),
+            state: net.state.clone(),
+        };
+        let (duplicate, duplicate_info) = super::peer::connect(
+            net.server.connect(addr, "localhost")?,
+            connection_ctxt,
+        );
+
+        let error = net
+            .add_active_peer(addr, duplicate, duplicate_info)
+            .unwrap_err();
+        assert_eq!(error.0, addr);
+        assert_eq!(net.get_active_peers().len(), 1);
+        drop(net);
+
+        let events = tokio::time::timeout(
+            Duration::from_secs(5),
+            info_rx.collect::<Vec<_>>(),
+        )
+        .await?;
+        assert_eq!(events.iter().filter(|(_, info)| info.is_none()).count(), 1);
+        Ok(())
     }
 
     #[tokio::test]
