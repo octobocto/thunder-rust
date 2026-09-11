c1a7e2dca6933991e4d6cfeedf84e6460ba87984
octobocto <bo@jalborg.com>
2026-09-11 08:54:42 -0700

    fix: 2WPD disconnect boundary skips the parent's event block

--- integration_tests/integration_test.rs
+++ integration_tests/integration_test.rs
@@ -13,7 +13,7 @@ use thunder_app_rpc_api::node::RpcClient as _;
 
 use crate::{
     block_template::block_template_trial,
-    ibd::ibd_trial,
+    ibd::{ibd_trial, reorg_across_deposit_trial},
     list_mempool::list_mempool_trial,
     receive_address::receive_address_trial,
     setup::{Init, PostSetup},
@@ -192,6 +192,11 @@ pub fn tests(
             failure_collector.clone(),
         ),
         transfer_many_trial(
+            bin_paths.clone(),
+            file_registry.clone(),
+            failure_collector.clone(),
+        ),
+        reorg_across_deposit_trial(
             bin_paths.clone(),
             file_registry.clone(),
             failure_collector.clone(),
